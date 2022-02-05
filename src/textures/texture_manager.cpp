6ff7c47810bb729b413abeb8e2a7fb3f15a2f3f3
0xSADCAT <alexsaltykow@gmail.com>
2022-02-06 02:10:00 +0300

    Texture manager for WindowImpl

---
+++ src/textures/texture_manager.cpp
@@ -0,0 +1,77 @@
+#include "texture_manager.hpp"
+
+#include <iostream>
+#include <cassert>
+#include <SDL2/SDL_image.h>
+
+#include "texture_path.hpp"
+
+
+TextureManager::TextureManager(SDL_Renderer* renderer)
+    : _renderer(renderer)
+{
+    assert(renderer != nullptr);
+
+    loadAll();
+}
+
+TextureManager::~TextureManager()
+{
+    this->free();
+}
+
+SDL_Texture* TextureManager::get(TextureId id) const
+{
+    if (static_cast<unsigned int>(id) >= static_cast<unsigned int>(TextureId::Undefined))
+    {
+        return nullptr;
+    }
+
+    return _textures[static_cast<unsigned int>(id)];
+}
+
+SDL_Texture* TextureManager::load(TextureId id)
+{
+    SDL_Texture* result = nullptr;
+
+    switch (id) {
+    case TextureId::Player:
+        result = IMG_LoadTexture(_renderer, texture_path::player.c_str());
+        break;
+
+    case TextureId::Undefined:
+        std::cerr << "[WARN] TextureManager::load(TextureId id): id = Undefined" << std::endl
+                  << "Loading UNDEFINED texture???" << std::endl;
+        break;
+    }
+
+    if (result == nullptr)
+    {
+        std::cerr << "[WARN] Failed to load texture:" << std::endl
+                  << IMG_GetError() << std::endl;
+    }
+    return result;
+}
+
+void TextureManager::loadAll()
+{
+    _textures.reserve(static_cast<unsigned int>(TextureId::Undefined));
+
+    _textures.push_back(load(TextureId::Player));
+
+    if (_textures.size() != static_cast<unsigned int>(TextureId::Undefined))
+    {
+        std::cerr << "[WARN] Loaded not all textures!" << std::endl;
+    }
+}
+
+void TextureManager::free()
+{
+    for (auto& texture : _textures)
+    {
+        if (texture != nullptr)
+        {
+            SDL_DestroyTexture(texture);
+        }
+    }
+}
---
+++ src/textures/texture_manager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+#include <SDL2/SDL.h>
+
+#include "texture_id.hpp"
+
+
+class TextureManager
+{
+public:
+    TextureManager(SDL_Renderer* renderer);
+    ~TextureManager();
+
+    SDL_Texture* get(TextureId id) const;
+
+private:
+    SDL_Texture* load(TextureId id);
+    void loadAll();
+    void free();
+
+    std::vector<SDL_Texture*> _textures;
+    SDL_Renderer* _renderer;
+
+    TextureManager(TextureManager&) = delete;
+    TextureManager(TextureManager&&) = delete;
+    TextureManager& operator=(TextureManager&) = delete;
+    TextureManager& operator=(TextureManager&&) = delete;
+};
