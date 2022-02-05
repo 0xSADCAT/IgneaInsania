ab4b3e389cb503b54f68c65a9573bcdc2683da25
0xSADCAT <alexsaltykow@gmail.com>
2022-02-05 20:59:41 +0300

    application.hpp - done
    Global application controll interface with events handling

--- src/application.hpp
+++ src/application.hpp
@@ -3,14 +3,16 @@
 #include <string>
 #include <vector>
 
-#include "ui/window.hpp"
+#include "structs/events.hpp"
 
 
 #define iapp \
     Application::instance()
 
 
-class ApplicationImpl;
+class Window;
+class Game;
+class ApplicationEventsImpl;
 
 
 class Application
@@ -22,6 +24,7 @@ public:
     static Application* instance();
 
     void setWindow(Window* window);
+    void setGameModel(Game* game);
 
     int execute();
 
@@ -29,12 +32,27 @@ public:
 
     const std::vector<std::string>& arguments() const;
 
+protected:
+    void mouseButtonEvent(const MouseButtonEvent &event);
+    void mouseMoveEvent(const MouseMoveEvent &event);
+    void mouseWheelEvent(const MouseWheelEvent &event);
+    void keyEvent(const KeyEvent &event);
+
+    void tickEvent(int microseconds_elapsed);
+
 private:
-    ApplicationImpl* _impl;
+    ApplicationEventsImpl* _events_impl;
+
+    std::vector<std::string> _arguments;
+
+    Window* _window = nullptr;
+    Game* _game = nullptr;
 
     Application(Application&) = delete;
     Application(Application&&) = delete;
     Application& operator=(Application&) = delete;
     Application& operator=(Application&&) = delete;
+
+    friend class ApplicationEventsImpl;
 };
 
