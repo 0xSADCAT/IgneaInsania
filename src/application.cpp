0a564447b0cce450b6f56d4aff0875b3b0f1c98f
0xSADCAT <alexsaltykow@gmail.com>
2022-02-05 21:05:00 +0300

    Application class .cpp file

---
+++ src/application.cpp
@@ -0,0 +1,109 @@
+#include "application.hpp"
+
+#include <cassert>
+#include "application_events_impl.hpp"
+#include "ui/window.hpp"
+#include "game/game.hpp"
+
+
+namespace
+{
+Application* instance = nullptr;
+}
+
+
+Application::Application(int argc, char** argv)
+    : _events_impl(new ApplicationEventsImpl)
+{
+    assert(::instance == nullptr);
+    ::instance = this;
+
+    _arguments.reserve(argc);
+    for (int i = 0; i < argc; ++i)
+    {
+        _arguments.push_back(argv[i]);
+    }
+}
+
+Application::~Application()
+{
+    ::instance = nullptr;
+    delete _events_impl;
+}
+
+Application* Application::instance()
+{
+    return ::instance;
+}
+
+void Application::setWindow(Window* window)
+{
+    _window = window;
+}
+
+void Application::setGameModel(Game* game)
+{
+    _game = game;
+}
+
+int Application::execute()
+{
+    return _events_impl->execute();
+}
+
+void Application::quit(int code)
+{
+    _events_impl->quit(code);
+}
+
+const std::vector<std::string>& Application::arguments() const
+{
+    return _arguments;
+}
+
+
+void Application::mouseButtonEvent(const MouseButtonEvent& event)
+{
+    if (_game != nullptr)
+    {
+        _game->onMouseButtonEvent(event);
+    }
+}
+
+void Application::mouseMoveEvent(const MouseMoveEvent& event)
+{
+    if (_game != nullptr)
+    {
+        _game->onMouseMoveEvent(event);
+    }
+}
+
+void Application::mouseWheelEvent(const MouseWheelEvent& event)
+{
+    if (_game != nullptr)
+    {
+        _game->onMouseWheelEvent(event);
+    }
+}
+
+void Application::keyEvent(const KeyEvent& event)
+{
+    if (_game != nullptr)
+    {
+        _game->onKeyEvent(event);
+    }
+}
+
+void Application::tickEvent(int microseconds_elapsed)
+{
+    if (_game != nullptr)
+    {
+        _game->onTickEvent(microseconds_elapsed);
+    }
+
+    if (_window != nullptr)
+    {
+        _window->clear();
+        _window->present();
+    }
+}
