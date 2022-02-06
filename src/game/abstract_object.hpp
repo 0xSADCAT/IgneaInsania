5baeea8dc4368bcd29461fc366f351fb6cc8eefe
0xSADCAT <alexsaltykow@gmail.com>
2022-02-06 12:11:04 +0300

    Abstract game object interface

---
+++ src/game/abstract_object.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../textures/texture_id.hpp"
+#include "../structs/geometry.hpp"
+
+
+class AbstractObject
+{
+public:
+    virtual ~AbstractObject() = default;
+
+    virtual Point position() const = 0;
+    /* temporary unused */ virtual Size size() const = 0;
+
+    virtual Size speed() const = 0;
+    virtual void setSpeed(const Size& speed) = 0;
+
+    virtual void move(int mcs) = 0;
+
+    virtual TextureId texture() const = 0;
+
+    virtual void setRotation(double value) = 0;
+    virtual double rotation() const = 0;
+
+    virtual void setFlip(Flip flip) = 0;
+    virtual Flip flip() const = 0;
+};
+
